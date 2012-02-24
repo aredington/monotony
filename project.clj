83bf2509d60599e79754fc1584c874361abeea31
Alex Redington <alex.redington@thinkrelevance.com>
2012-02-24 12:05:14 -0500

    Add lein-clojars dev dependency for pushing real good.

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
                  [org.clojure/core.logic "0.6.7"]
                  [org.clojure/math.numeric-tower "0.0.1"]
                  [fidjet "0.0.1"]]
-  :dev-dependencies [[com.stuartsierra/lazytest "2.0.0-SNAPSHOT"]]
+  :dev-dependencies [[com.stuartsierra/lazytest "2.0.0-SNAPSHOT"]
+                     [lein-clojars "0.6.0"]]
   :repositories {"stuartsierra-releases" "http://stuartsierra.com/maven2"
                  "stuartsierra-snapshots" "http://stuartsierra.com/m2snapshots"})
