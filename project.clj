21053fcb9b0399ee7ac27d3b7af10bc370dc3ce7
Alex Redington <alex.redington@thinkrelevance.com>
2012-10-26 10:12:18 -0400

    Update project.clj to work with lein2 (again)

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
                  [org.clojure/core.logic "0.6.7"]
                  [org.clojure/math.numeric-tower "0.0.1"]
                  [fidjet "0.0.1"]]
-  :profiles {:dev  {:dependencies [[com.stuartsierra/lazytest "2.0.0-SNAPSHOT"]
-                                   [lein-clojars "0.6.0"]]}}
+  :profiles {:dev  {:dependencies [[com.stuartsierra/lazytest "2.0.0-SNAPSHOT"]]
+                    :plugins [[lein-clojars "0.9.1"]]}}
   :repositories {"stuartsierra-releases" "http://stuartsierra.com/maven2"
                  "stuartsierra-snapshots" "http://stuartsierra.com/m2snapshots"})
