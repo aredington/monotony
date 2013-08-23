9b8b5370167a1165696de65dbb5b435c03b87075
Alex Redington <alex.redington@thinkrelevance.com>
2013-08-23 11:53:00 -0400

    Uncovered a bug in fidjet
    - Multiple arities break
    - Destructured items that still resolve to the with-foo name break
    - OH NO WHY IS EVERYTHING BREAKING?!

--- test/monotony/test/configured.clj
+++ test/monotony/test/configured.clj
@@ -8,16 +8,13 @@
 
 (def start-of-2011-gmt (t/date 1293840000000))
 
+(def first-day-of-2011 (c/period-after test-conf start-of-2011-gmt :day))
+
 (def start-of-second-week-in-2011-gmt (t/date 1293926400000))
 
 (def a-milli-in-2011 (t/date 1316802232642))
 
-(facts "about `next-boundary`"
-       (fact "generates dates correctly with next-boundary in a with-config block"
+(facts "about `period-name`"
+       (fact "correctly identifies periods"
              (with-config test-conf
-               (next-boundary start-of-2011-gmt :week)) => start-of-second-week-in-2011-gmt))
-
-(facts "about `prior-boundary`"
-          (fact "generates dates correctly with prior-boundary in a with-config block"
-                (with-config test-conf
-                  (prior-boundary a-milli-in-2011 :year)) => start-of-2011-gmt))
+               (period-name first-day-of-2011)) => :saturday))
